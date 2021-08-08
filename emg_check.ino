3bff01db698cb206431a60d749189354aaff7e82
unknown <firoskhansha@gmail.com>
2021-08-08 22:38:48 +0530

    eng checker

---
+++ emg_check.ino
@@ -0,0 +1,24 @@
+#define EMGPin0 A0
+#define EMGPin1 A1
+int EMGVal0;
+int EMGVal1;
+
+void setup() {
+Serial.begin(9600);
+}
+
+void loop() {
+  
+EMGVal0 = analogRead(EMGPin0);
+
+EMGVal1 = analogRead(EMGPin1);
+
+Serial.print(EMGVal0);
+Serial.print(",");
+Serial.print(EMGVal1);
+delay(500);
+
+//print new line
+Serial.println();
+
+}
